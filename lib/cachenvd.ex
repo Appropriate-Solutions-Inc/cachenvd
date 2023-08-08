761a449d644e0b0c3394cfcd86fa81441604bfab
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-08-08 19:08:44 -0400

    Base mix project

---
+++ lib/cachenvd.ex
@@ -0,0 +1,18 @@
+defmodule Cachenvd do
+  @moduledoc """
+  Documentation for `Cachenvd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cachenvd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cachenvd_test.exs
@@ -0,0 +1,8 @@
+defmodule CachenvdTest do
+  use ExUnit.Case
+  doctest Cachenvd
+
+  test "greets the world" do
+    assert Cachenvd.hello() == :world
+  end
+end
