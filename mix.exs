e0cfc5cd68f53ae244f25827f3cca54f3637719f
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-08-15 10:16:25 -0400

    Reset mix.exs to 0.0.0 for early dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cachenvd.MixProject do
   def project do
     [
       app: :cachenvd,
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
