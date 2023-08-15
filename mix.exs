bb5e0d864cf86caab52d999bc1b34c77b734124d
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-08-15 10:18:30 -0400

    Add tbump

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Cachenvd.MixProject do
   use Mix.Project
 
+  @version "0.0.0"
+
   def project do
     [
       app: :cachenvd,
-      version: "0.0.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
