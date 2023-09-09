ce8128773136a3b449e91349f345e68653ad8091
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-09-09 08:09:27 -0400

    Update packages

--- mix.exs
+++ mix.exs
@@ -27,11 +27,11 @@ defmodule Cachenvd.MixProject do
       {:bandit, ">=0.7.7"},
       {:nimble_csv, "~> 1.2"},
       {:amnesia, "~> 0.2.8"},
-      {:req, "~> 0.3.0"},
+      {:req, "~> 0.4.0"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:stream_data, "~> 0.6", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
