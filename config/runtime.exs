b1a42cd22f28f4ceac3a7dea8dbc88c16dec598f
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-09-09 08:37:58 -0400

    Runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,3 +1,6 @@
 import Config
 
-config :cachenvd, :keys, nvd_api_key: System.get_env("NVD_API_KEY")
+config :cachenvd,
+  nvd_api_key: System.get_env("NVD_API_KEY")
+
+# import_config "#{config_env()}.exs"
